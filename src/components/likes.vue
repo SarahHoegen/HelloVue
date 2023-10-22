beb5626830e231fb7b093ef7668e454cb9a829cc
SarahHoegen <sarihosan@gmail.com>
2023-10-22 19:03:57 -0300

    feat: First Commit

---
+++ src/components/likes.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const contador = ref(0)
+
+function incrementarContador() {
+  contador.value++
+}
+
+function decrementarContador() {
+  if (contador.value > 0){
+    contador.value-- 
+  }
+}
+
+function resetarContador() {
+  contador.value = 0
+}
+
+onMounted(() => {
+  console.log(`O valor inicial do contador é ${contador.value}.`)
+})
+</script>
+
+<template>
+  <button @click="incrementarContador">Incrementar</button>
+  <button @click="decrementarContador">Decrementar</button>
+  <p>Valor do contador é: {{ contador }}</p>
+  <button @click="resetarContador">Resetar</button>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
---
+++ src/components/sintaxe.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const numero = 1
+    const ok = true
+    const mensagem = 'programar'
+    const url = 'https://vuejs.org/'
+</script>
+
+<template>
+  {{ numero + 1 }}
+  {{ ok ? 'Sim' : 'Não' }}
+  {{ mensagem.split('').reverse().join('') }}
+  <br>
+    <a :href="url">Mais informações...</a>
+</template>
